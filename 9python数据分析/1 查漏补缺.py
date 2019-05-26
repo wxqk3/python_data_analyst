0ee9d8a4278c004c90c0e76c8149c4172b8338e1
William Xia <wxqk3@mail.missouri.edu>
2019-05-26 15:25:49 -0500

    第二阶段最后一节9python数据分析

---
+++ 9python数据分析/1 查漏补缺.py
@@ -0,0 +1,25 @@
+#coding:utf8
+
+#第一章 1-4----------------
+#1 多行注释用三引号
+
+#2 区分大小写
+
+a=u'{0}年{1}月{2}日'.format('2017',3,24)
+
+print a
+
+b=format(1000000,'e')
+'''科学计数法'''
+
+'''格式化输出'''
+print('%s %s' % ('hello world','hello'))
+
+
+
+#第一章 5-8---------------- list,tuple,dict,序列及分片，set  https://blog.csdn.net/business122/article/details/7541486
+
+dict={1:1,2:2}
+dict2={'1':1,2:2}
+#潜复制 不影响值
+#dict3=dict 则指向同一对象
